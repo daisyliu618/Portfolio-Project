30d02f51fbdcbfd71bae3a52217234be88535a0f
Daisy <daisyliudesign@gmail.com>
2020-10-11 05:11:46 -0400

    update css

---
+++ dist/js/test.js
@@ -0,0 +1,8 @@
+let curs = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+    curs.style.left = x - 15 + "px";
+    curs.style.top = y - 15 + "px";
+  });
